23332edcaaed4505204bd27a6d1710316333f3f7
sajicode <sajioloye@gmail.com>
2020-03-07 13:17:08 +0100

    setup wallet create & fund wallet endpoints

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require('express-validator');
 const { config } = require('dotenv');
 const Rave = require('../helpers/flutterwave');
 const { authUser } = require('../middleware/auth');
+const Wallet = require('../models/Wallet');
 
 config();
 
@@ -75,6 +76,11 @@ router.post(
 		check('otp', 'The OTP is required').notEmpty().isString()
 	],
 	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ status: 'fail', errors: errors.array() });
+		}
+
 		const { transaction_reference, otp } = req.body;
 
 		try {
@@ -87,6 +93,73 @@ router.post(
 			res.status(200).send(response.data);
 		} catch (error) {
 			res.status(400).send({ status: 'fail', message: error.message });
+		}
+	}
+);
+
+/* 
+	create user wallet endpoint
+*/
+router.post(
+	'/create-wallet',
+	[ check('userID', 'The User ID is required').notEmpty().isString() ],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			console.log(errors.array());
+			return res.status(400).json({ status: 'fail', errors: errors.array() });
+		}
+
+		const userID = req.body.userID;
+
+		const wallet = {
+			userID
+		};
+
+		try {
+			const newWallet = Wallet.create(wallet);
+			return res.status(200).send({ status: 'success', data: newWallet });
+		} catch (error) {
+			console.log('Wallet creation error', error.message);
+			return res.status(500).send({ status: 'fail', message: error.message });
+		}
+	}
+);
+
+/*
+fund wallet endpoint
+*/
+
+router.post(
+	'/fund-wallet',
+	authUser,
+	[ check('amount', 'Wallet fund amount is required').notEmpty().isNumeric() ],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ status: 'fail', errors: errors.array() });
+		}
+
+		const user_id = req.user.id;
+		const amount = req.body.amount;
+
+		const userWallet = await Wallet.findOne({ userID: user_id });
+
+		if (!userWallet) {
+			return res.status(404).send({ status: 'fail', message: 'User has no wallet' });
+		}
+
+		try {
+			const wallet = await Wallet.findOneAndUpdate(
+				{ userID: user_id },
+				{ $inc: { ledgerBalance: amount, availableBalance: amount } },
+				{ new: true }
+			);
+
+			return res.status(200).send({ status: 'success', data: wallet });
+		} catch (error) {
+			console.log('Wallet fund error', error.message);
+			return res.status(500).send({ status: 'fail', message: error.message });
 		}
 	}
 );
